0e7767e0058497e8e8fb0ddf6c4196e88e8502ef
Andre Ravazzi <amravazzi@gmail.com>
2022-11-11 09:58:50 -0300

    feat: update app styles and structure. Sorry for the delay

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Lorem Ipsum Blog</h1>
+        <button>Novo Post</button>
+        <button>Dark Mode</button>
       </header>
+      <main>
+        <section className="post-card">
+          <div className="post-preview-content">
+            <div className="post-author">
+              <img
+                src="https://avatars.dicebear.com/api/adventurer/your-custom-seed.svg"
+                alt="Avatar do autor"
+                className="author-avatar"
+              />
+              <p>Fulana</p>
+            </div>
+            <div className="post-body">
+              <p className="post-title">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+              <p className="post-preview">
+                Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                Etiam faucibus vel neque in hendrerit. Praesent congue augue in
+                quam rutrum, at bibendum mauris posuere. Integer quis eros
+                vestibulum, commodo sapien eget, posuere purus. Morbi erat nisl,
+                scelerisque nec tortor id, interdum iaculis orci. Suspendisse
+                sit amet lacus sit amet orci lobortis cursus. Integer egestas
+                pulvinar mauris sed luctus. Nulla in eros odio. Donec et ante
+                lacus. Curabitur et sollicitudin odio. Nam at nunc in est
+                condimentum malesuada dictum porta velit. Praesent a luctus
+                lectus. Phasellus sed mi a nibh fermentum sagittis. Curabitur
+                sit amet auctor mi. Suspendisse potenti. Mauris maximus
+                consequat mi a mollis.
+              </p>
+            </div>
+          </div>
+          <div className="post-image">
+            <img src="https://picsum.photos/100" alt="imagem do post" />
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
