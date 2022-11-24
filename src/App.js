69bb7d60a6980ffd7ea9657e93fb1662f46d5da7
Andre Ravazzi <amravazzi@gmail.com>
2022-11-24 20:47:02 -0300

    Aula 13 ou 14

--- src/App.js
+++ src/App.js
@@ -3,30 +3,55 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [theme, setTheme] = useState("day");
+  const [isPostOpen, setIsPostOpen] = useState(false);
+  const [currentPostIndex, setCurrentPostIndex] = useState("");
 
   useEffect(() => fetchPosts(), []);
 
   useEffect(() => console.log(posts), [posts]);
 
   function fetchPosts() {
+    // fetch ~= axios
+    // <Promise>.then().catch() <=> try-catch
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((data) => {
         data.length = 10;
         setPosts(data);
-      });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleTheme() {
+    setTheme((theme) => (theme === "night" ? "day" : "night"));
+  }
+
+  function tooglePostModal() {
+    setIsPostOpen((prev) => !prev);
+  }
+
+  function handleOpenPost(postIndex) {
+    tooglePostModal();
+    setCurrentPostIndex(postIndex);
   }
 
   return (
-    <div className="main-container">
+    <div className="main-container" data-theme={theme}>
       <header>
         <h1>Lorem Ipsum Blog</h1>
-        <button>Novo Post</button>
-        <button>Dark Mode</button>
+        <button className="btn primary">Novo Post</button>
+        <button className="btn primary" onClick={() => handleTheme()}>
+          {theme === "night" ? "Day" : "Night"} Mode
+        </button>
       </header>
       <main>
-        {posts.map((post) => (
-          <section className="post-card" id={post.id}>
+        {posts.map((post, i) => (
+          <section
+            className="post-card"
+            id={post.id}
+            onClick={() => handleOpenPost(i)}
+          >
             <div className="post-preview-content">
               <div className="post-author">
                 <img
@@ -50,6 +75,21 @@ function App() {
           </section>
         ))}
       </main>
+
+      {isPostOpen ? (
+        <section className="fullscreen-post">
+          <button
+            onClick={() => tooglePostModal()}
+            className="btn secondary post-close"
+          >
+            &times;
+          </button>
+          <div className="post-content">
+            <h1>{posts[currentPostIndex].title}</h1>
+            <p>{posts[currentPostIndex].body}</p>
+          </div>
+        </section>
+      ) : null}
     </div>
   );
 }
